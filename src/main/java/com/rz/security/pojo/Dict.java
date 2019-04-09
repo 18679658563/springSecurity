a55099412a7ec428753cf2079e14fff2fd49132c
silence <m18679658563@163.com>
2019-04-09 13:59:55 +0800

    test -dict

--- src/main/java/com/rz/security/pojo/Dict.java
+++ src/main/java/com/rz/security/pojo/Dict.java
@@ -1,37 +1,18 @@
 package com.rz.security.pojo;
 
+import lombok.Data;
+
 /***
  * 字典类
  */
+@Data
 public class Dict extends BaseEntity<String> {
 
 	private static final long serialVersionUID = -2431140186410912787L;
 	private String type;
-	private String k;
-	private String val;
+	private String key;
+	private String value;
 
-	public String getType() {
-		return type;
-	}
 
-	public void setType(String type) {
-		this.type = type;
-	}
-
-	public String getK() {
-		return k;
-	}
-
-	public void setK(String k) {
-		this.k = k;
-	}
-
-	public String getVal() {
-		return val;
-	}
-
-	public void setVal(String val) {
-		this.val = val;
-	}
 
 }
